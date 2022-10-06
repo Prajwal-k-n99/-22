c626bcc627aa27474bd31a33d174af4896d5793b
Prajwal K N <prajwalkpura99@gmail.com>
2022-10-06 21:20:17 +0530

    constructors

---
+++ src/Abstraction.java
@@ -0,0 +1,23 @@
+abstract class Pen{
+    abstract void write();
+    abstract void refill();
+}
+ class FountainPen extends Pen{
+    void write(){
+        System.out.println("Write");
+    }
+    void refill(){
+        System.out.println("Refill");
+    }
+    void changeNib(){
+        System.out.println("Changing the nib");
+    }
+}
+public class Abstraction {
+    public static void main(String[] args) {
+        FountainPen f = new FountainPen();
+        f.changeNib();
+        f.write();
+        f.refill();
+    }
+}
